83b3315a18174569b78915eb09f4a88a9438ba24
Yann Rochard <yann.rochard@gmail.com>
2011-05-19 18:04:50 +0200

    Redefinition de to_s + exemple.

--- Vehicle.rb
+++ Vehicle.rb
@@ -1,5 +1,22 @@
 #! /usr/bin/ruby                            
 
 class Car
+
   attr_accessor :name, :brand
+  
+  def to_s
+    "Brand : #{brand}, Name: #{name}"
+  end
+  
 end
+
+
+
+
+# CARS
+car1 = Car.new()
+car1.name = "Astra"
+car1.brand = "Opel"
+puts "Car definition : #{car1}"
+# "#{car1}" est équivalent à la méthode to_s c-a-d : "Brand : #{brand}, Name: #{name}"
+                  
