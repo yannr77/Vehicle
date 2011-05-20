88999b0d08b28a24aa426a826c58d6cbc4282359
Yann Rochard <yann.rochard@gmail.com>
2011-05-20 10:14:27 +0200

    Vérification qu'un conducteur est un adulte avant de le laisser conduire

--- Vehicle.rb
+++ Vehicle.rb
@@ -3,26 +3,33 @@
 class Car
 
   attr_accessor :name, :brand, :driver
-  
+
   def to_s
-    "Brand : #{brand}, Name: #{name}"
+    "Brand : #{brand}, Name: #{name}. "
   end
   
-  def driver
-    @driver
+  def driver=(new_driver)
+    if new_driver.can_drive? 
+      @driver = new_driver 
+    else
+      puts "#{new_driver} is too young."
+    end
   end
-  
-  def can_drive?(passenger)
-    @driver = passenger if passenger.age > 18
-  end
-  
-  
+    
 end
 
 
 class Passenger
   attr_accessor :name, :age
 
+  def to_s
+    "#{name} (#{age} years old)"
+  end
+  
+  def can_drive?
+    age > 18
+  end
+  
 end
 
 
@@ -31,13 +38,18 @@ car1 = Car.new()
 car1.name = "Astra"
 car1.brand = "Opel"
 puts "Car definition : #{car1}"
+
 # "#{car1}" est équivalent à la méthode to_s c-a-d : "Brand : #{brand}, Name: #{name}"
  
 #PASSENGERS    
 p1 = Passenger.new()
 p1.name = "Yann"             
 p1.age = 19
+car1.driver = car1
 
 p2 = Passenger.new()
 p2.name = "Nicolas"             
 p2.age = 15
+car1.driver = p2
+
+puts "The driver is #{car1.driver}"
