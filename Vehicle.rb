d508e1a4ed9103409e33a43ec6b7cb4adebcf0d3
Yann Rochard <yann.rochard@gmail.com>
2011-05-20 10:48:18 +0200

    Test sur le nombre de passengers, les affichers etc

--- Vehicle.rb
+++ Vehicle.rb
@@ -2,7 +2,7 @@
 
 class Car
 
-  attr_accessor :name, :brand, :driver
+  attr_accessor :name, :brand, :driver, :count_passengers
 
   def to_s
     "Brand : #{brand}, Name: #{name}. "
@@ -15,15 +15,27 @@ class Car
       puts "#{new_driver} is too young."
     end
   end
-    
+  
+  def count_passengers_car(thenumber)
+    if thenumber.count_passengers?
+      puts"The number of passengers is correct"
+    else
+      puts"You are too many !"
+    end
+  end 
+  
 end
 
 
 class Passenger
   attr_accessor :name, :age
 
+  def count_passengers?
+  passenger.count < 5 
+  end
+
   def to_s
-    "#{name} (#{age} years old)"
+    "#{name} (#{age} years old) "
   end
   
   def can_drive?
@@ -37,19 +49,31 @@ end
 car1 = Car.new()
 car1.name = "Astra"
 car1.brand = "Opel"
-puts "Car definition : #{car1}"
-
+car1.count_passengers = []
+puts "Car1 definition : #{car1}"
 # "#{car1}" est équivalent à la méthode to_s c-a-d : "Brand : #{brand}, Name: #{name}"
+ 
+car2 = Car.new()
+car2.name = "A5"
+car2.brand = "Audi"
+car2.count_passengers = 6
+puts "Car2 definition : #{car2}"
  
 #PASSENGERS    
 p1 = Passenger.new()
 p1.name = "Yann"             
 p1.age = 19
-car1.driver = car1
 
 p2 = Passenger.new()
 p2.name = "Nicolas"             
 p2.age = 15
-car1.driver = p2
 
-puts "The driver is #{car1.driver}"
+
+car1.driver = p1
+car2.driver = p2
+puts "The car1's driver is #{car1.driver}"
+puts "The car2's driver is #{car2.driver}."
+
+car1.count_passengers  = [car1.driver, p2]
+
+puts "CAR 1 : The number of passagers is: #{car1.count_passengers.size}. They are: #{car1.count_passengers}"
