d273359d97b14d3d01e2b4951d38ced3f025df62
Yann Rochard <yann.rochard@gmail.com>
2011-05-23 10:23:37 +0200

    Création d'un people p1 et d'une voiture car1 avec p1 driver de car1

--- Vehicle2.rb
+++ Vehicle2.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/ruby
 
 class Car
-  attr_accessor :name, :brand
+  attr_accessor :name, :brand, :driver
   
   
   def to_s
@@ -11,7 +11,18 @@ class Car
 end
 
 
-class people
+class People
 attr_accessor :name, :age
 
 end
+
+p1 = People.new()
+p1.name = "Yann"
+p1.age = 19
+
+car1 = Car.new()
+car1.name = "Astra"
+car1.brand = "Opel"
+car1.driver = p1
+
+puts "Le conducteur de car1 est: #{car1.driver.name}"
